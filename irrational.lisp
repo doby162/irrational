00c18f683d9400e7e4a3209542be8a42245e7a4d
Michael Acker <doby162@gmail.com>
2016-11-10 14:45:26 -0600

    making weird changes

--- irrational.lisp
+++ irrational.lisp
@@ -1,20 +1,20 @@
-(let ((n 871) (x 1))
-(dotimes (k 100)
-(setf x (* 0.5 (+ x (/ n x)))))
-(format t "~A" x))
+;(let ((n 871) (x 1))
+;(dotimes (k 90)
+;(setf x (* 0.5 (+ x (/ n x)))))
+;(format t "~100$" x))
+;(base64:BASE64-STRING-TO-INTEGER "hello")
+;(ql:quickload :cl-base64)
+
+(defun code-2 (digits)
+  (isqrt (* 2 (expt 10 (* 2 (- digits 2))))))
 
 
+(defun arbitrary-root ( &optional (n 2) (prec 100))
+  (isqrt (* n (expt 10 (* 2 (- prec 2))))))
 
 
-;(let ((digits 10000) (result 1) (remainder 1))
-;  (dotimes (i digits)
-;    (* remainder 100)
-;    (let ((j 0))
-;      (loop
-;        (when (and (<= (* j (+ j (* 20 result))) remainder) (> (* (+ 20 result j 1)  (+ j 1)) remainder))
-;          (break))
-;        (setf j (+ j 1))
-;        (setf remainder (- remainder (
-;((20 * result + (j + 1)) * (j + 1)) > remainder:
-
-;(20 * result + (j + 1))
+(defun test ()
+  (and
+    (= (arbitrary-root) 141421356237309504880168872420969807856967187537694807317667973799073247846210703885038753432764157)
+    (= (arbitrary-root 2 10) 141421356)
+    (= (arbitrary-root 3 8) 17320508)))
